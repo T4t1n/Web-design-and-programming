62e1833a27814e5a55475842dbb4fdbf475193dc
T4t1n <jaimiskytn@gmail.com>
2025-10-28 17:14:29 -0600

    Upload

--- Projects/19-Landing page/js/script.js
+++ Projects/19-Landing page/js/script.js
@@ -1,7 +1,21 @@
-const faqQuestion = document.querySelectorAll('.faq-question');
+const faqQuestions = document.querySelectorAll('.faq-question');
 
 faqQuestions.forEach(question => {
     question.addEventListener('click', () => {
-        
-    })
-})
+        faqQuestions.forEach(item => {
+            if (item !== question) {
+                item.classList.remove('active');
+                item.nextElementSibling.style.maxHeight = null;
+            }
+        });
+
+        question.classList.toggle('active');
+
+        const answer = question.nextElementSibling;
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
+    });
+});
